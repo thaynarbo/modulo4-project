a3cdd582ec869766fc59474423c5b327684b1ae3
thaynar96 <thaynarbo@gmail.com>
2021-10-14 16:47:27 -0300

    feat(app): add AuthModule as provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { GenreModule } from './genre/genre.module';
 import { ProfileModule } from './profile/profile.module';
 import { GameModule } from './game/game.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [GenreModule, ProfileModule, UsersModule, GameModule],
+  imports: [GenreModule, ProfileModule, UsersModule, GameModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
