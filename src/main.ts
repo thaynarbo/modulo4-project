f79273657d833df25b07163f9a4fae559067f6af
thaynar96 <thaynarbo@gmail.com>
2021-10-14 15:53:56 -0300

    feature(cors): enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://localhost:3001', 'http://localhost:3002'],
+    },
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
