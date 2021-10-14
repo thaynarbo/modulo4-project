145a9f8f664705056e9407e5a596d6490786f9a6
thaynar96 <thaynarbo@gmail.com>
2021-10-14 15:58:28 -0300

    feat(service): implement crud on all endpoints

--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 import { CreateGenreDto } from './dto/create-genre.dto';
 import { UpdateGenreDto } from './dto/update-genre.dto';
 
 @Injectable()
 export class GenreService {
-  create(createGenreDto: CreateGenreDto) {
-    return 'This action adds a new genre';
+  constructor(private readonly prisma: PrismaService) {}
+  create(data: CreateGenreDto) {
+    return this.prisma.genre.create({
+      data,
+    });
   }
 
   findAll() {
-    return `This action returns all genre`;
+    return this.prisma.genre.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} genre`;
+    return this.prisma.genre.findUnique({
+      where: { id },
+    });
   }
 
-  update(id: number, updateGenreDto: UpdateGenreDto) {
-    return `This action updates a #${id} genre`;
+  update(id: number, data: UpdateGenreDto) {
+    return this.prisma.genre.update({
+      where: { id },
+      data,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} genre`;
+    return this.prisma.genre.delete({
+      where: { id },
+    });
   }
 }
