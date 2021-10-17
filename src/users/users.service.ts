f7eed1cf43280f0ec4e73f0ced32fb9b9b1f9786
thaynar96 <thaynarbo@gmail.com>
2021-10-17 11:00:45 -0300

    feat(user):implement findById and findByEmail

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,39 @@
+import { Prisma } from '.prisma/client';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
-  create(data: CreateUserDto) {
-    return this.prisma.user.create({ data });
+  // password precisará ser criptografado então passamos ele pelo bcrypt
+  async create(dto: CreateUserDto) {
+    const data: Prisma.UserCreateInput = {
+      ...dto,
+      password: await bcrypt.hash(dto.password, 10),
+    };
+    const createdUser = await this.prisma.user.create({ data });
+    return {
+      ...createdUser,
+      password: undefined,
+    };
   }
 
   findAll() {
     return this.prisma.user.findMany();
   }
 
-  findOne(id: number) {
+  findById(id: number) {
     return this.prisma.user.findUnique({
       where: { id },
+    });
+  }
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
     });
   }
 
