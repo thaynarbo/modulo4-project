27ac5432972c23ca409abb97caa464a6aa576e8e
thaynar96 <thaynarbo@gmail.com>
2021-10-17 11:47:13 -0300

    feat(auth):implement login and JWT sign

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './model/LoginDTO';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
-
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+import { LoginDto } from './model/LoginDTO';
+import * as bcrypt from 'bcrypt';
+import { UserPayload } from './model/UserPayload';
+import { UserToken } from './model/UserToken';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+  async login(dto: LoginDto): Promise<UserToken> {
+    const user: User = await this.validateUser(dto.email, dto.password);
+    const payload: UserPayload = {
+      sub: user.id,
+      username: user.email,
+    };
+  }
+  validateUser(email: string, password: string): User | PromiseLike<User> {
+    const user: User = await this.usersService.findByEmail(email);
+
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (isPasswordValid) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+    }
+    throw new Error('User and/or password is not valid');
+  }
+}
