9adb5c4ab3eff7ffd4847b2dcc3e64968a180dcc
thaynar96 <thaynarbo@gmail.com>
2021-10-20 23:53:04 -0300

    feat(profile): adjust dto

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -8,16 +8,13 @@ import {
 } from 'class-validator';
 import { Profile } from '../entities/profile.entity';
 export class CreateProfileDto extends Profile {
-  @IsString({ message: `Your nickname can't contain numbers` })
-  @IsNotEmpty({ message: `Your nickname can't be empty` })
+  @IsString()
+  @IsNotEmpty()
   nickname: string;
 
-  @IsString({ message: `The image must be a string` })
-  image: string;
+  @IsString()
+  image?: string;
 
-  @IsObject()
-  user: Prisma.UserCreateNestedOneWithoutProfilesInput;
-
-  @IsOptional()
-  games?: Prisma.GameCreateNestedManyWithoutProfileInput;
+  @IsNumber()
+  userId: number;
 }
